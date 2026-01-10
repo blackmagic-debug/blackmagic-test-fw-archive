d06c5c9be98da32036a93a7d2e58bf18cc9c99ff
dragonmux <git@dragonmux.network>
2026-01-10 13:51:33 +0000

    stm32/f4/serial: Added the build.rs that was missing so linking works correctly

---
+++ stm32/f4/serial/build.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+fn main()
+{
+    println!("cargo:rustc-link-arg-bins=--nmagic");
+    println!("cargo:rustc-link-arg-bins=-Tlink.x");
+    println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+}
